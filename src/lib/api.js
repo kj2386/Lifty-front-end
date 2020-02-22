2e45d8c421da0c91a089435899b99fa12b843cca
kj2386 <kj2386@gmail.com>
2020-02-21 18:40:33 -0700

    edited and created fetchs for exercise

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,43 +1,56 @@
-import { APIURL } from "../config";
+import { APIURL } from '../config';
+
 //GET workout list
 export const fetchWorkoutList = () => {
   return fetch(`${APIURL}/workout`).then(resp => resp.json());
 };
+
 //GET specific workout
 export const fetchWorkout = workoutid => {
   return fetch(`${APIURL}/workout/${workoutid}`).then(resp => resp.json());
 };
+
 //DELETE set
 export const deleteSet = (exerciseId, setId) => {
   return fetch(`${APIURL}/exercise/${exerciseId}/${setId}`, {
-    method: "delete"
-  }).then(res => res.json());
-}
-//DELETE workout
-export const deleteWorkout = (workoutId) => {
-  return fetch(`${APIURL}/workout/${workoutId}`, {
-    method: "delete"
+    method: 'delete'
   }).then(res => res.json());
 };
 
-//UPDATE set
-export const updateExercise = (exerciseId, body) => {
-  return fetch(`${APIURL}/exercise/${exerciseId}`, {
-    method: "PUT",
-    body: JSON.stringify(body)
+//DELETE workout
+export const deleteWorkout = workoutId => {
+  return fetch(`${APIURL}/workout/${workoutId}`, {
+    method: 'delete'
   }).then(res => res.json());
 };
-//CREATE exercise
-export const createExercise = body => {
-  return fetch(`${APIURL}/exercise`, {
-    method: "POST",
-    body: JSON.stringify(body)
-  }).then(res => res.json());
-};
+
 //CREATE workout
 export const createWorkout = body => {
   return fetch(`${APIURL}/workout`, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify(body)
+  }).then(res => res.json());
+};
+
+//CREATE exercise
+export const createExercise = body => {
+  return fetch(`${APIURL}/exercise`, {
+    method: 'POST',
+    body: JSON.stringify(body)
+  }).then(res => res.json());
+};
+
+//UPDATE exercise in a workout
+export const updateExercise = (exerciseId, workoutId, body) => {
+  return fetch(`${APIURL}/workout/${workoutId}/${exerciseId}`, {
+    method: 'PUT',
+    body: JSON.stringify(body)
+  }).then(res => res.json());
+};
+
+//DELETE an exercise
+export const deleteExercise = exerciseId => {
+  return fetch(`${APIURL}/exercise/${exerciseId}`, {
+    method: 'delete'
   }).then(res => res.json());
 };
